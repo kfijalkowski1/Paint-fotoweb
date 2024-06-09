0092b862fdabeefb6557cb623f2596478c1e08a9
Adam Jeliński <adas.jelinski@gmail.com>
2024-06-09 19:52:54 +0200

    add pinia and layouts

--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -6,8 +6,9 @@
 
 // Plugins
 import vuetify from './vuetify'
+import pinia from '@/stores'
 import router from '@/router'
 
 export function registerPlugins(app) {
-    app.use(vuetify).use(router)
+    app.use(vuetify).use(router).use(pinia)
 }

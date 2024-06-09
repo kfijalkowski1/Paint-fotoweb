37a33c9ab962910eb1624ac60138315c281660fa
Adam Jeliński <adas.jelinski@gmail.com>
2024-06-09 19:48:11 +0200

    add formatter configuration

--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -8,8 +8,6 @@
 import vuetify from './vuetify'
 import router from '@/router'
 
-export function registerPlugins (app) {
-  app
-    .use(vuetify)
-    .use(router)
+export function registerPlugins(app) {
+    app.use(vuetify).use(router)
 }

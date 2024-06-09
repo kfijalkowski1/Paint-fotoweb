33659ea0a73ec0426c9996a9f6b5623a9ccdb82f
Adam Jeliński <adas.jelinski@gmail.com>
2024-06-10 00:18:46 +0200

    add top navigation

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+const links = [
+    { text: 'Strona główna', href: '/' },
+    { text: 'Oferta', href: '/offer' },
+    { text: 'Odbierz zdjęcia', href: '/photos' },
+    { text: 'Kontakt', href: '/contact' },
+]
+</script>
 <template>
     <v-app-bar
         :elevation="2"
@@ -14,7 +21,35 @@
         <v-app-bar-title>Fotoweb</v-app-bar-title>
 
         <template v-slot:append>
-            <v-app-bar-nav-icon color="secondary"></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon
+                class="d-md-none"
+                color="secondary"
+            ></v-app-bar-nav-icon>
+            <div class="d-none d-md-flex">
+                <v-divider
+                    vertical
+                    :thickness="2"
+                    class="mx-2 border-opacity-75"
+                />
+                <span
+                    v-for="link in links"
+                    :key="link.text"
+                    class="d-flex flex-grow"
+                >
+                    <RouterLink
+                        :to="link.href"
+                        class="text-secondary text-decoration-none"
+                        exactActiveClass="font-weight-black"
+                    >
+                        {{ link.text }}
+                    </RouterLink>
+                    <v-divider
+                        vertical
+                        :thickness="2"
+                        class="mx-2 border-opacity-75"
+                    />
+                </span>
+            </div>
         </template>
     </v-app-bar>
 </template>

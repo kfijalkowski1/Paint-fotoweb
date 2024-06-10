e234f4d41938e16a791412f0a5aa6b72f3b3b4b3
Adam Jeliński <adas.jelinski@gmail.com>
2024-06-10 18:11:33 +0200

    final photo download page

--- frontend/src/pages/photos.vue
+++ frontend/src/pages/photos.vue
@@ -3,17 +3,42 @@ import { ref } from 'vue'
 
 const accessCode = ref('')
 
-const photos = ref([
-    { url: 'aaa' },
-    { url: 'aaa' },
-    { url: 'aaa' },
-    { url: 'aaa' },
-])
+const photos = ref([])
+const allPhotos = ref('')
+const errorMessage = ref('')
 
-const show = () => {
-    // sprawdź dostęp -> może wyświetl błąd
-    // pobierz dane
-    // wyświetl wszystko
+const show = async () => {
+    photos.value = []
+    allPhotos.value = ''
+    errorMessage.value = ''
+
+    try {
+        const fileList = await fetch(
+            `${import.meta.env.VITE_FILE_SERVER_URL}/getFileList/${
+                accessCode.value
+            }`
+        ).then((data) => data.json())
+
+        const zipFileName = fileList.find((file) =>
+            file.match(/^.*\.(zip|ZIP)$/)
+        )
+
+        allPhotos.value =
+            zipFileName &&
+            `${import.meta.env.VITE_FILE_SERVER_URL}/files/${
+                accessCode.value
+            }/${zipFileName}`
+
+        photos.value = fileList
+            .filter((file) => !file.match(/^.*\.(zip|ZIP)$/))
+            .map((file) => ({
+                url: `${import.meta.env.VITE_FILE_SERVER_URL}/files/${
+                    accessCode.value
+                }/${file}`,
+            }))
+    } catch {
+        errorMessage.value = 'Ten kod dostępu jest nieprawidłowy'
+    }
 }
 </script>
 <template>
@@ -29,13 +54,6 @@ const show = () => {
                 class="d-flex flex-column ga-8 align-center justify-center fill-height"
             >
                 <span class="text-h3 text-white">Odbierz zdjęcia</span>
-                <v-btn
-                    variant="outlined"
-                    color="white"
-                >
-                    Więcej
-                    <v-icon icon="mdi-arrow-down"></v-icon>
-                </v-btn>
             </div>
         </v-img>
 
@@ -44,6 +62,8 @@ const show = () => {
                 v-model="accessCode"
                 label="Kod dostępu"
                 required
+                :error="errorMessage !== ''"
+                :error-messages="errorMessage"
             ></v-text-field>
 
             <v-btn
@@ -61,14 +81,16 @@ const show = () => {
         >
             <v-divider class="mb-4" />
             <v-btn
+                v-if="allPhotos"
                 class="flex-grow-1 mb-4"
                 color="green"
-                @click="downloadAll"
+                download
+                :href="allPhotos"
             >
                 Pobierz wszystkie
             </v-btn>
 
-            <div class="d-flex flex-wrap justify-space-evenly">
+            <div class="d-flex flex-wrap justify-space-evenly ga-4">
                 <v-card
                     v-for="photo in photos"
                     :key="photo.url"
@@ -76,12 +98,18 @@ const show = () => {
                 >
                     <v-img
                         height="200px"
-                        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                        :src="photo.url"
                         cover
                     ></v-img>
 
                     <template v-slot:actions>
-                        <v-btn class="flex-grow-1">Pobierz</v-btn>
+                        <v-btn
+                            class="flex-grow-1"
+                            download
+                            :href="photo.url"
+                        >
+                            Pobierz
+                        </v-btn>
                     </template>
                 </v-card>
             </div>

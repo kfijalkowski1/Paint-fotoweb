dc2b37521792ec34a93e41dc1f4d7d4af6ec1dfd
gambolkf <gambolkf@gmail.com>
2024-06-10 03:02:08 +0200

    feat: supa POC

---
+++ frontend/src/pages/index-supa-poc.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '@/supabase/supabaseClient'
+
+const countries = ref([])
+
+async function getSlots() {
+  const { data } = await supabase.from('slots').select()
+  countries.value = data
+  console.log("aaaaa")
+  console.log(data)
+}
+
+onMounted(() => {
+  getSlots()
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="slot in slots" :key="slot.id">{{ slot.start_time }}</li>
+  </ul>
+</template>
